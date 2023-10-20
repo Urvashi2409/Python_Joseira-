9dd4b9008c615946532c1de2364de82202cb041d
Urvashi Anand <139621372+Urvashi2409@users.noreply.github.com>
2023-10-20 21:56:12 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,53 @@
+# print("Urvashi Anand 123 44353535 7.8 %325#%352355@~@~!##$@$")
+
+# name = input("Enter your name : ")
+# print(name)
+
+
+# Data types in python  
+# int = 23, 34, 352352352352355, 49320702364263042342346 
+# float = 34.56, 90.67242342374234238 
+# string = "Urvashi" "sjdfgsjdffgkdsfksdfksfjsfdjsadjasdasfdjasfdsadafskdasdsfdkfksfdksdd"
+# 'Urvashi', 
+# """Urvashi"""
+# '''Urvashi'''
+# char = 'A' 'C' 'E' 
+# bool = True, False 
+
+# grade = '''A'''
+# print(type(grade))
+
+# print(type(True))
+# print(type(45.6))
+# print(type("45.6"))
+
+# print(type(45))
+
+# variable : 
+
+# 45 = "Urvashi"
+
+# variable naming conventions 
+# 1. uppercase and lowercase alphabets are possible everywhere 
+# 2. cannot start a variable name with a number (NEVER DO THIS), but anywhere else is possible
+# 3. underscore is only possible symbol everywhere 
+# 4. we cannot use spaces in variable name 
+# 5. we cannot use the reserved keyword in a variable 
+
+# syntax 
+# print = "Urvashi"
+# input = "Urvashi"
+
+# conditional statements 
+# age = int(input("Enter your age: "))
+# if age > 18:
+#     print("You can vote")
+# elif age == 18:
+#     print("You need to go apply for voter id")
+# else:
+#     print("You cannot vote")
+
+# for loops, while loops 
+# functions 
+# strings 
+# list, tuples, dictionary 
